cc750d413f3fe097563eaf1cbedb6c0e12f3089b
ryanweiler92 <ryanweiler92@gmail.com>
2023-08-07 19:10:06 -0400

    update links for embed mode instead of distraction free

--- src/App.tsx
+++ src/App.tsx
@@ -23,39 +23,39 @@ const environments = [
 const scenarios = [
   {
     title: 'MODIS Corrected Reflectance',
-    url: '?v=-181.74360912131363,-98.53068072538338,182.80846643543055,106.52986177528524&df=true&kiosk=true&eic=si&l=OrbitTracks_Terra_Descending(opacity=0.9),Coastlines_15m,MODIS_Terra_CorrectedReflectance_TrueColor&lg=false'
+    url: '?v=-181.74360912131363,-98.53068072538338,182.80846643543055,106.52986177528524&em=true&kiosk=true&eic=si&l=OrbitTracks_Terra_Descending(opacity=0.9),Coastlines_15m,MODIS_Terra_CorrectedReflectance_TrueColor&lg=false'
   },
   {
     title: 'VIIRS Nighttime Black Marble',
-    url: '?v=-181.74360912131363,-98.53068072538338,182.80846643543055,106.52986177528524&df=true&kiosk=true&eic=si&l=Coastlines_15m(opacity=0.63),VIIRS_SNPP_DayNightBand_At_Sensor_Radiance&lg=true'
+    url: '?v=-181.74360912131363,-98.53068072538338,182.80846643543055,106.52986177528524&em=true&kiosk=true&eic=si&l=Coastlines_15m(opacity=0.63),VIIRS_SNPP_DayNightBand_At_Sensor_Radiance&lg=true'
   },
   {
     title: 'Subdaily GeoColor',
-    url: '?v=-218.05641352247375,-98.53068072538338,146.49566203427042,106.52986177528524&ics=true&ici=5&icd=10&df=true&kiosk=true&eic=si&l=Coastlines_15m(opacity=0.77),GOES-East_ABI_GeoColor,GOES-West_ABI_GeoColor,Himawari_AHI_Band3_Red_Visible_1km&lg=false'
+    url: '?v=-218.05641352247375,-98.53068072538338,146.49566203427042,106.52986177528524&ics=true&ici=5&icd=10&em=true&kiosk=true&eic=si&l=Coastlines_15m(opacity=0.77),GOES-East_ABI_GeoColor,GOES-West_ABI_GeoColor,Himawari_AHI_Band3_Red_Visible_1km&lg=false'
   },
   {
     title: 'VIIRS Fires',
-    url: '?v=-181.74360912131363,-98.53068072538338,182.80846643543055,106.52986177528524&df=true&kiosk=true&eic=si&l=Coastlines_15m,VIIRS_SNPP_Thermal_Anomalies_375m_Day,VIIRS_SNPP_CorrectedReflectance_TrueColor&lg=true'
+    url: '?v=-181.74360912131363,-98.53068072538338,182.80846643543055,106.52986177528524&em=true&kiosk=true&eic=si&l=Coastlines_15m,VIIRS_SNPP_Thermal_Anomalies_375m_Day,VIIRS_SNPP_CorrectedReflectance_TrueColor&lg=true'
   },
   {
     title: 'Precipitation Rate',
-    url: '?v=-181.74360912131363,-98.53068072538338,182.80846643543055,106.52986177528524&df=true&kiosk=true&eic=si&l=IMERG_Precipitation_Rate,Land_Mask&lg=false'
+    url: '?v=-181.74360912131363,-98.53068072538338,182.80846643543055,106.52986177528524&em=true&kiosk=true&eic=si&l=IMERG_Precipitation_Rate,Land_Mask&lg=false'
   },
   {
     title: 'Sea Surface Temperature',
-    url: '?v=-181.74360912131363,-98.53068072538338,182.80846643543055,106.52986177528524&df=true&kiosk=true&eic=si&l=Coastlines_15m(opacity=0.71),GHRSST_L4_MUR_Sea_Surface_Temperature(palette=divergent_1)&lg=true'
+    url: '?v=-181.74360912131363,-98.53068072538338,182.80846643543055,106.52986177528524&em=true&kiosk=true&eic=si&l=Coastlines_15m(opacity=0.71),GHRSST_L4_MUR_Sea_Surface_Temperature(palette=divergent_1)&lg=true'
   },
   {
     title: 'Land Surface Temperature',
-    url: '?v=-181.74360912131363,-98.53068072538338,182.80846643543055,106.52986177528524&df=true&kiosk=true&eic=si&l=Coastlines_15m,MODIS_Aqua_Land_Surface_Temp_Day,MODIS_Aqua_CorrectedReflectance_TrueColor(opacity=0.8)&lg=true'
+    url: '?v=-181.74360912131363,-98.53068072538338,182.80846643543055,106.52986177528524&em=true&kiosk=true&eic=si&l=Coastlines_15m,MODIS_Aqua_Land_Surface_Temp_Day,MODIS_Aqua_CorrectedReflectance_TrueColor(opacity=0.8)&lg=true'
   },
   {
     title: 'Sea Ice Arctic Animation',
-    url: '?v=-4171187.01916682,-4238559.180936186,4142545.3786389525,4188800.5389352706&p=arctic&df=true&kiosk=true&eic=da&l=Land_Mask,AMSRU2_Sea_Ice_Concentration_12km(palette=blue_6)&lg=true'
+    url: '?v=-4171187.01916682,-4238559.180936186,4142545.3786389525,4188800.5389352706&p=arctic&em=true&kiosk=true&eic=da&l=Land_Mask,AMSRU2_Sea_Ice_Concentration_12km(palette=blue_6)&lg=true'
   },
   {
     title: 'Sea Ice Antarctic Animation',
-    url: '?v=-4171187.01916682,-4238559.180936186,4142545.3786389525,4188800.5389352706&p=antarctic&df=true&kiosk=true&eic=da&l=Land_Mask,AMSRU2_Sea_Ice_Concentration_12km(palette=blue_6)&lg=true'
+    url: '?v=-4171187.01916682,-4238559.180936186,4142545.3786389525,4188800.5389352706&p=antarctic&em=true&kiosk=true&eic=da&l=Land_Mask,AMSRU2_Sea_Ice_Concentration_12km(palette=blue_6)&lg=true'
   }
 ]
 
